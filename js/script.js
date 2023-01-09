d2cf3b456d6d6af8ff7d23351a6003f1d3053ad8
Leo9723 <pala9761@gmail.com>
2023-01-09 14:03:15 +0100

    utilizzo di mounted anziché di methods per portare a termine l'esercizio

--- js/script.js
+++ js/script.js
@@ -7,13 +7,11 @@ createApp({
             NewEmail: []
         }
     },
-    methods: {
-        CreateEmail(){
-            for(let i=0; i<10; i++){
-                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                    this.NewEmail.push(result.data.response);
-                });
-            }
+    mounted() {
+        for(let i=0; i<10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                this.NewEmail.push(result.data.response);
+            });
         }
-    }
+    },
 }).mount('#app')
